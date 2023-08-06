520a5bfc4f0a54ee8f69284f3d201b5c96bcf359
jaeywon <dnsjwodyd@gmail.com>
2023-08-06 22:02:19 +0900

    wordpress오류까지 확인완료

--- inception/srcs/req/wordpress/tools/wordpress_setup.sh
+++ inception/srcs/req/wordpress/tools/wordpress_setup.sh
@@ -4,10 +4,9 @@ set -e
 
 if [ ! -f /var/www/html/index.php ]; then
   chown -R www-data:www-data /var/www/html
-
-  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  chmod +x wp-cli.phar
-  mv wp-cli.phar /usr/local/bin/wp
+  echo $MYSQL_DATABASE
+  echo $MYSQL_USER
+  echo $MYSQL_PASSWORD
   wp core download --locale=ko_KR --allow-root
   wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
   wp core install --url=https://jaeywon.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
