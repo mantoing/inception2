30e8edcdba2119301ddc1c1d34037d91a79289a0
jaeywon <dnsjwodyd@gmail.com>
2023-08-07 21:31:00 +0900

    mariadb 전까지

--- inception/srcs/req/mariaDB/sets/mariadb_setup.sh
+++ inception/srcs/req/mariaDB/sets/mariadb_setup.sh
@@ -4,6 +4,9 @@ set -e
 
 MYSQL_SETUP_FILE=/var/lib/mysql/.setup
 
+chown -R 777 /var/lib/mysql/
+chown -R mysql:mysql /var/lib/mysql/
+
 service mariadb start
 
 if [ ! -e $MYSQL_SETUP_FILE ]; then
@@ -12,10 +15,11 @@ if [ ! -e $MYSQL_SETUP_FILE ]; then
 	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
 	mysql -e "FLUSH PRIVILEGES";
 	mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
+	# echo "complete"
 	mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
 	mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
 
 	touch $MYSQL_SETUP_FILE
 fi
 
-exec /usr/bin/mysqld_safe
+exec mysqld --console
