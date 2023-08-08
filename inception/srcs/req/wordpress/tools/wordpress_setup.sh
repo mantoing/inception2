5e9fe12fe1e0971800c65511100e6d3cda2ecf3d
jaeywon <dnsjwodyd@gmail.com>
2023-08-08 20:52:35 +0900

    가상머신 직전까지 끝

--- inception/srcs/req/wordpress/tools/wordpress_setup.sh
+++ inception/srcs/req/wordpress/tools/wordpress_setup.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+chown -R www-data:www-data /var/www/html
+
 if [ ! -f /var/www/html/index.php ]; then
-  chown -R www-data:www-data /var/www/html
   wp core download --locale=ko_KR --allow-root
-  wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
-  wp core install --url=https://jaeywon.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+  echo "Running wp config create..."
+  wp config create --dbname=mariadb --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+  echo "Running wp config finished..."
+  wp core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
   wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
 fi
-
+    
 exec "php-fpm7.4" "-F"
