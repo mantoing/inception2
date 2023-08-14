db05b61b274d3e7e00de2bb81b2e4056d410c295
jaeywon <dnsjwodyd@gmail.com>
2023-08-14 20:35:53 +0900

    최종본

--- inception/srcs/req/mariaDB/sets/mariadb_setup.sh
+++ inception/srcs/req/mariaDB/sets/mariadb_setup.sh
@@ -17,12 +17,8 @@ fi
 if [ ! -e $MYSQL_SETUP_FILE ]; then
 	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
 	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
-	# mysql -e "GRANT ALL ON *.* TO 'root'@'localhost' identifided by '$MYSQL_ROOT_PASSWORD'";
 	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
-	# mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
 	mysql -e "FLUSH PRIVILEGES";
-	# mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
-	# mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
 	touch $MYSQL_SETUP_FILE
 fi
 
--- inception/srcs/req/wordpress/tools/wordpress_setup.sh
+++ inception/srcs/req/wordpress/tools/wordpress_setup.sh
@@ -6,10 +6,8 @@ chown -R www-data:www-data /var/www/html
 
 if [ ! -f /var/www/html/index.php ]; then
   wp core download --locale=ko_KR --allow-root
-  echo "Running wp config create..."
   wp config create --dbname=mariadb --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
-  echo "Running wp config finished..."
-  wp core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+  wp core install --url=https://jaeywon.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
   wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
 fi
     
